4c7150bb3730e24fc2c21ecc672802252299db69
Buabeng Emmanuel Aikins <67046324+aikins23@users.noreply.github.com>
2025-09-04 10:50:03 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,25 +1,24 @@
--- Initial Query: Retrieve all bookings with user, property, and payment details
+-- Initial query to retrieve all bookings with user, property, and payment details
 SELECT 
     b.booking_id,
     b.start_date,
     b.end_date,
     b.total_price,
-    b.status,
     u.user_id,
-    u.first_name,
-    u.last_name,
+    u.username,
     u.email,
     p.property_id,
     p.name AS property_name,
     p.location,
     pay.payment_id,
     pay.amount,
-    pay.payment_method,
+    pay.status AS payment_status,
     pay.payment_date
 FROM bookings b
-JOIN users u ON b.user_id = u.user_id
-JOIN properties p ON b.property_id = p.property_id
-LEFT JOIN payments pay ON b.booking_id = pay.booking_id;
+INNER JOIN users u ON b.user_id = u.user_id
+INNER JOIN properties p ON b.property_id = p.property_id
+INNER JOIN payments pay ON b.booking_id = pay.booking_id;
+
 
 -- Analyze performance using EXPLAIN
 EXPLAIN
