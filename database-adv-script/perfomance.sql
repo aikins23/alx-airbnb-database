945e2ec38264f6df3e64704d8d476e5ad19ba67b
Buabeng Emmanuel Aikins <67046324+aikins23@users.noreply.github.com>
2025-09-04 10:57:21 +0000

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -14,7 +14,10 @@ SELECT
 FROM bookings b
 JOIN users u ON b.user_id = u.user_id
 JOIN properties p ON b.property_id = p.property_id
-JOIN payments pay ON b.booking_id = pay.booking_id;
+JOIN payments pay ON b.booking_id = pay.booking_id
+WHERE b.status = 'confirmed'
+AND pay.status = 'completed';
+
 
 
 
